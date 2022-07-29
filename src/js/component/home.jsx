ffa5ffa793e20c55749dd44ab8bea0cf210b385a
oscq12 <orianasofia86@gmail.com>
2022-07-29 19:58:02 +0000

    parctica todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex justify-content-center">
+			<div className="row">
+				<h1>HOLA</h1>
+				<div className="col-12 md-6">
+
+					<input placeholder="que quieres hacer?"></input>
+
+				</div>
+
+			</div>
+			
 		</div>
 	);
 };
